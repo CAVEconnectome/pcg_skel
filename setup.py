4c04c6fdb315f6bd1f0fdeea18365064ff6fd2d8
Casey Schneider-Mizell <caseysm@gmail.com>
2021-02-11 14:13:26 -0800

    add long description

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import codecs
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 def read(*parts):
     with codecs.open(os.path.join(here, *parts), 'r') as fp:
@@ -22,7 +25,6 @@ def find_version(*file_paths):
 
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
-
 dependency_links = []
 del_ls = []
 for i_l in range(len(required)):
@@ -41,6 +43,8 @@ setuptools.setup(
     author="Casey Schneider-mizell",
     author_email="caseys@alleninstitute.org",
     description="Skeletonization using the pychunkedgraph",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=required,
     include_package_data=True,
     dependency_links=dependency_links,
