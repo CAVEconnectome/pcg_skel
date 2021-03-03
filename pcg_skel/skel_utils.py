8a5f9980a18151fda2286ddd834df45a4f1b114e
Casey Schneider-Mizell <caseysm@gmail.com>
2021-03-02 17:26:25 -0800

    Fix l2dict utility function

--- pcg_skel/skel_utils.py
+++ pcg_skel/skel_utils.py
@@ -3,10 +3,8 @@ import warnings
 
 
 def filter_l2dict(sk_ch, l2dict_mesh_r):
-    """Converts a reverse level 2 dict for a mesh into one for a skeleton
-    """
-    l2dict_sk_r = {ii: l2dict_mesh_r[mind]
-                   for ii, mind in enumerate(sk_ch.mesh_index)}
+    """Converts a reverse level 2 dict for a mesh into one for a skeleton"""
+    l2dict_sk_r = {ii: l2dict_mesh_r[mind] for ii, mind in enumerate(sk_ch.mesh_index)}
     l2dict_sk = {v: k for v, k in l2dict_sk_r.items()}
     return l2dict_sk, l2dict_sk_r
 
@@ -15,14 +13,12 @@ def propagate_l2dict(sk_ch, l2dict_mesh):
     """Propagate a l2dict for a mesh to an l2dict for a skeleton, with multiple
     level 2 ids potentially mapping to each skeleton index
     """
-    l2dict_long = {l2dict_mesh[k]: sk_ch.mesh_to_skel_map[v]
-                   for k, v in l2dict_mesh.items()}
+    l2dict_long = {k: sk_ch.mesh_to_skel_map[v] for k, v in l2dict_mesh.items()}
     return l2dict_long
 
 
 def fix_nan_verts(sk, num_rounds=20):
-    """ Replace vertices with locations that are nan with mean of neighbor locations
-    """
+    """Replace vertices with locations that are nan with mean of neighbor locations"""
     nr = 0
     while nr < num_rounds:
 
@@ -30,15 +26,17 @@ def fix_nan_verts(sk, num_rounds=20):
         new_verts = sk.vertices[nanvinds]
 
         for ii, vid in enumerate(nanvinds):
-            neib_inds = np.array(
-                sk.csgraph_binary_undirected[vid, :].todense()).squeeze() > 0
+            neib_inds = (
+                np.array(sk.csgraph_binary_undirected[vid, :].todense()).squeeze() > 0
+            )
             if len(neib_inds) == 0:
                 continue
             if np.any(neib_inds):
                 with warnings.catch_warnings():
                     warnings.simplefilter("ignore")
                     new_verts[ii] = np.nanmean(
-                        sk.vertices[neib_inds], axis=0) + np.array([0, 0, 1])
+                        sk.vertices[neib_inds], axis=0
+                    ) + np.array([0, 0, 1])
         sk._rooted._vertices[nanvinds] = new_verts
         sk._vertices[nanvinds] = new_verts
 
@@ -47,7 +45,7 @@ def fix_nan_verts(sk, num_rounds=20):
 
         nr += 1
     else:
-        print(f'Could not fix all nans after {num_rounds} rounds')
+        print(f"Could not fix all nans after {num_rounds} rounds")
     pass
 
 
@@ -55,7 +53,7 @@ def get_centered_mesh(mesh):
     if mesh is None:
         return np.array([np.nan, np.nan, np.nan])
     verts = mesh.vertices
-    close_ind = np.argmin(np.linalg.norm(verts-np.mean(verts, axis=0), axis=1))
+    close_ind = np.argmin(np.linalg.norm(verts - np.mean(verts, axis=0), axis=1))
     return verts[close_ind]
 
 
